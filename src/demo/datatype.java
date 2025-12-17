1014ca248b32eda870e0cf6f443978d1d53aaadc
ayyanshaikh1 <skayyan193@gmail.com>
2025-12-17 16:28:58 +0530

    add the data type file

---
+++ src/demo/datatype.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class datatype {
+	public static void main(String [] args) {
+		int a = 0b1011;
+		System.out.println(a);
+		
+		int b = 0x45;  
+		System.out.println(b);
+		
+		int c = 45;
+		System.out.println(c);
+		
+		int d = 045;
+		System.out.println(d);
+	}
+}
