b5c77392409cc40ff1cea5aac18a8df54d39a7d6
ayyanshaikh1 <skayyan193@gmail.com>
2025-12-18 16:52:43 +0530

    add the boolean, special character, type Casting and registration Form

--- src/demo/datatype.java
+++ src/demo/datatype.java
@@ -2,16 +2,16 @@ package demo;
 
 public class datatype {
 	public static void main(String [] args) {
-		int a = 0b1011;
+		int a = 0b1011;  // 11
 		System.out.println(a);
 		
-		int b = 0x45;  
+		int b = 0x45;  //  69
 		System.out.println(b);
 		
-		int c = 45;
+		int c = 45;   //45
 		System.out.println(c);
 		
-		int d = 045;
+		int d = 045;  //37
 		System.out.println(d);
 	}
 }
