545b7b48ca78480f71b5130ac7dda7dadfa81eca
ayyanshaikh1 <skayyan193@gmail.com>
2025-12-27 22:40:35 +0530

    in student program error fix

--- src/Encapculation/Student.java
+++ src/Encapculation/Student.java
@@ -3,43 +3,44 @@ package Encapculation;
 import java.util.Scanner;
 
 public class Student {
-
-	public static void main(String[] args) {
+	public static void main(String [] args)
+	{
 		Scanner sc = new Scanner(System.in);
-		Student s = new Student();
+		StudentInfo s1 = new StudentInfo();
 		System.out.println("Enter the Name :");
 		String name = sc.nextLine();
-		System.out.println("Your Name :" +s.getName());
+		System.out.println("Enter the age :");
+		int age = sc.nextInt();
+		sc.nextLine();
 		
-		System.out.println("Enter your Age : ");
-		int age =sc.nextInt();
+		System.out.println("Enter the Gender :");
+		String gender = sc.nextLine();
+		s1.setData(name, age,gender);
 		
-		System.out.println("Your Age :" +s.getName());	
-		
-				
+		System.out.println("Name :" +s1.getName());
+		System.out.println("Age :" +s1.getAge());
+		System.out.println("Gender :" +s1.getGender());
 	}
+	
 }
 
-class Student1{
+class StudentInfo{
 	private String name;
 	private int age;
 	private String gender;
 	
-	public void setData(String n, int a, String gen) {
+	public void setData(String n, int a, String g) {
 		name = n;
 		age = a;
-		gender = gen;
+		gender = g;
 	}
 	public String getName() {
 		return name;
 	}
-	
-	public int getAge(int age){
+	public int getAge() {
 		return age;
 	}
-
-	public string getGender(String gen) {
+	public String getGender(){
 		return gender;
 	}
-	
 }
